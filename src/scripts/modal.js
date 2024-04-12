0edab3891cbc17739caadeba99a80a919309739c
Maia-th <thiagomaia633@gmail.com>
2024-04-12 17:05:25 -0300

    style: Indentação dos codigos

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,30 +1,30 @@
 // Obtém os elementos das imagens e dos modais
-var imagensObra = document.querySelectorAll('.imagemObra');
-var modais = document.querySelectorAll('.modal');
+var imagensObra = document.querySelectorAll(".imagemObra");
+var modais = document.querySelectorAll(".modal");
 
 // Adiciona eventos de clique para exibir os modais
-imagensObra.forEach(function(imagem, index) {
-  imagem.onclick = function() {
-    modais[index].style.display = 'block';
-  }
+imagensObra.forEach(function (imagem, index) {
+  imagem.onclick = function () {
+    modais[index].style.display = "block";
+  };
 });
 
 // Obtém os elementos dos botões fechar
-var closeBtns = document.querySelectorAll('.close');
+var closeBtns = document.querySelectorAll(".close");
 
 // Adiciona eventos de clique para fechar os modais
-closeBtns.forEach(function(btn) {
-  btn.onclick = function() {
+closeBtns.forEach(function (btn) {
+  btn.onclick = function () {
     var modal = btn.parentElement.parentElement;
-    modal.style.display = 'none';
-  }
+    modal.style.display = "none";
+  };
 });
 
 // Fecha os modais se o usuário clicar fora deles
-window.onclick = function(event) {
-  modais.forEach(function(modal) {
+window.onclick = function (event) {
+  modais.forEach(function (modal) {
     if (event.target == modal) {
-      modal.style.display = 'none';
+      modal.style.display = "none";
     }
   });
-}
+};
